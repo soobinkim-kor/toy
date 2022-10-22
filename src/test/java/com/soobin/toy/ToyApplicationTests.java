a199966be3f61154f0b7424f3e3025c31e1a5489
soobinkim-kor <shb03207@naver.com>
2022-10-22 14:42:20 +0900

    Initial commit

---
+++ src/test/java/com/soobin/toy/ToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soobin.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
